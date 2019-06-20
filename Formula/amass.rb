fcb9d6d81fd31bae2e23b09d1db5c2afa3d42317
caffix <caffix@users.noreply.github.com>
2019-06-20 19:47:01 -0400

    Brew formula update for Amass version 3.0.13

--- Formula/amass.rb
+++ Formula/amass.rb
@@ -2,14 +2,14 @@
 class Amass < Formula
   desc "In-depth DNS Enumeration and Network Mapping"
   homepage "https://www.owasp.org/index.php/OWASP_Amass_Project"
-  version "3.0.9"
+  version "3.0.13"
 
   if OS.mac?
-    url "http://github.com/OWASP/Amass/releases/download/3.0.9/amass_3.0.9_macos_amd64.zip"
-    sha256 "84f9a5fe2e66064cf35a08d062513a05d9e9020bc0ec3a92e185a0832a880270"
+    url "http://github.com/OWASP/Amass/releases/download/3.0.13/amass_3.0.13_macos_amd64.zip"
+    sha256 "4d5a07e9ad30f12510535c6ccae0623e43332bb5c880b6fc3f938ddc0aa64996"
   elsif OS.linux?
-    url "http://github.com/OWASP/Amass/releases/download/3.0.9/amass_3.0.9_macos_amd64.zip"
-    sha256 "e2145db3e05fd98f29cb3f4c26f1a194a62bd418cccee4c4c80153dbdb59bef4"
+    url "http://github.com/OWASP/Amass/releases/download/3.0.13/amass_3.0.13_macos_amd64.zip"
+    sha256 "7935a32357d0ca658b9c1c7e062f0ff1713425ae68c0d25eb4d2d2ee94dadf0f"
   end
 
   def install
