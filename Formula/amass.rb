af28eb95e5203a91a4166f1900509ed390247a2f
caffix <caffix@users.noreply.github.com>
2019-08-01 18:37:41 -0400

    Brew formula update for Amass version v3.0.26

--- Formula/amass.rb
+++ Formula/amass.rb
@@ -2,15 +2,15 @@
 class Amass < Formula
   desc "In-depth DNS Enumeration and Network Mapping"
   homepage "https://www.owasp.org/index.php/OWASP_Amass_Project"
-  version "3.0.25"
+  version "3.0.26"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/OWASP/Amass/releases/download/v3.0.25/amass_v3.0.25_macos_amd64.zip"
-    sha256 "be7b881cdbdb589bd13a0ca3e8fecf8bc4c8fcb46f8461f7fd3144ee9bddf1cb"
+    url "https://github.com/OWASP/Amass/releases/download/v3.0.26/amass_v3.0.26_macos_amd64.zip"
+    sha256 "58dc937b8e6d0f2e4d047fd3a7e3dd826921a70bb48599a25886db26afc9bb58"
   elsif OS.linux?
-    url "https://github.com/OWASP/Amass/releases/download/v3.0.25/amass_v3.0.25_linux_amd64.zip"
-    sha256 "3d30987f3b56821ffdc1b7cc5da8002f60e99882b0ed71e35c61e25eed7d22a9"
+    url "https://github.com/OWASP/Amass/releases/download/v3.0.26/amass_v3.0.26_linux_amd64.zip"
+    sha256 "94552349cda371e45fb50f51a2805abb4d0d30fd70bdfd3b04655e544f2c4a06"
   end
 
   def install
