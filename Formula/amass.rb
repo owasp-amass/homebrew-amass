8a2f5ab6d8705ccd33166df2c31ae4160c44814e
caffix <caffix@users.noreply.github.com>
2021-02-15 03:05:55 -0500

    Brew formula update for amass version v3.11.5

--- Formula/amass.rb
+++ Formula/amass.rb
@@ -5,24 +5,24 @@
 class Amass < Formula
   desc "In-depth Attack Surface Mapping and Asset Discovery"
   homepage "https://owasp.org/www-project-amass/"
-  version "3.11.4"
+  version "3.11.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/OWASP/Amass/releases/download/v3.11.4/amass_macos_amd64.zip"
-    sha256 "3a02ed517606611b89422c767222698f8807dac4c661c3e4a42a2d34e152599d"
+    url "https://github.com/OWASP/Amass/releases/download/v3.11.5/amass_macos_amd64.zip"
+    sha256 "f44a394e555bb1a5518c8e1209a9041efe2c3998fc8fd8f1bd4f7b5fb8a4ba94"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/OWASP/Amass/releases/download/v3.11.4/amass_linux_amd64.zip"
-    sha256 "523c3d5b1189894d20c7fffcfcd82ce81cf9e3b5e7ceebbb1ffb80557d28f3a5"
+    url "https://github.com/OWASP/Amass/releases/download/v3.11.5/amass_linux_amd64.zip"
+    sha256 "7c668b540ee1818d6d297d91db617684cc43da018c41b96148e1a9935b4a5d6b"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/OWASP/Amass/releases/download/v3.11.4/amass_linux_arm.zip"
-    sha256 "d5483e60d0bbbaabdf471ad0a73d561790387c5465621d4527f1e40d490d7977"
+    url "https://github.com/OWASP/Amass/releases/download/v3.11.5/amass_linux_arm.zip"
+    sha256 "ffbccea2f8d68652cd5023946c717aab8c350d7ca9b5f7fa433c4208bfb8e1b7"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/OWASP/Amass/releases/download/v3.11.4/amass_linux_arm64.zip"
-    sha256 "446592fa788f64d20ef84a5c7408dfcebc3540fd0e74e3366edc511792d2c266"
+    url "https://github.com/OWASP/Amass/releases/download/v3.11.5/amass_linux_arm64.zip"
+    sha256 "a29377f6a200dfdc5778125f219e81d561bb2288127dd1f7726dd0a2b7edb1c9"
   end
 
   def install
